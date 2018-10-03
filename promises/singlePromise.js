6c88c4030cddc6feee544fdea811da1fee5a8847
Johknotter Hill <johknotterhill@gmail.com>
2018-10-03 16:14:54 -0400

    more promises practice

--- promises/singlePromise.js
+++ promises/singlePromise.js
@@ -1,1 +1,4 @@
-console.log("SANITY CHECK!")
+// console.log("SANITY CHECK!");
+const apiKey = `e9ddb24aed6d48c4342303aba5269e28`;
+const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
+const imgUrl = `http://image.tmdb.org/t/p/w300/`;
